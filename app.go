52d8652c8cefc2f17ef7ebc4cf174da1c5e2397c
fliaping <269832017@qq.com>
2021-10-17 00:35:34 +0800

    主动休眠，解决某些未知问题导致的不能休眠

--- app.go
+++ app.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"encoding/hex"
+	"fmt"
 	wapi "github.com/iamacarpet/go-win64api"
+	"github.com/robfig/cron"
 	"log"
 	"net"
+	"os/exec"
 	"strings"
+	"time"
 )
 
 func getMacAddr() ([]string, error) {
@@ -22,18 +26,52 @@ func getMacAddr() ([]string, error) {
 	}
 	return as, nil
 }
-
+func mrdConnected() bool {
+	out, err := exec.Command("powershell", "[void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms');[System.Windows.Forms.SystemInformation]::TerminalServerSession").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var output string = string(out)
+	return strings.HasPrefix(output, "True")
+}
 func main() {
 	log.Print("service start")
+	var latestWolTime int64 = time.Now().Unix()
+	//开启休眠定时
+	c := cron.New() //精确到秒
+	//定时任务
+	spec := "* */1 * * * ?" //cron表达式，每秒一次
+	err := c.AddFunc(spec, func() {
+		mrdCon := mrdConnected()
+		timeout := (time.Now().Unix() - latestWolTime) > 15*60 //15分钟没收到WOL即超时
+		if !mrdCon && timeout {
+			log.Printf("go sleep, mrdConnected%s,timeout:%s\n", mrdCon, timeout)
+			//rundll32.exe powrprof.dll,SetSuspendState 0,1,0
+			c := exec.Command("powershell", "rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+			if err := c.Run(); err != nil {
+				fmt.Println("Error: ", err)
+			}
+		}
+	})
+	if err != nil {
+		return
+	}
+	c.Start()
 
 	pc, err := net.ListenPacket("udp4", ":9")
 	if err != nil {
 		panic(err)
 	}
-	defer pc.Close()
+	defer func(pc net.PacketConn) {
+		err := pc.Close()
+		if err != nil {
+			log.Printf("[ERROR] ListenPacket error,%s\n", err.Error())
+		}
+	}(pc)
 
 	buf := make([]byte, 1024)
 
+	// 从udp循环读取数据
 	for {
 		n, addr, err := pc.ReadFrom(buf)
 		if err != nil {
@@ -44,6 +82,7 @@ func main() {
 
 		isWol := isWolPackage(packageHex)
 		if isWol {
+			latestWolTime = time.Now().Unix()
 			res, err := wapi.SetThreadExecutionState(wapi.ES_SYSTEM_REQUIRED)
 			if res != 0 {
 				log.Printf("[ERROR] reset sleep timer fail,%s\n", err.Error())
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module DoNotSleep
 
 go 1.16
 
-require github.com/iamacarpet/go-win64api v0.0.0-20210311141720-fe38760bed28
+require (
+	github.com/iamacarpet/go-win64api v0.0.0-20210311141720-fe38760bed28
+	github.com/robfig/cron v1.2.0
+)
